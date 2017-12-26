1f7f394e7adc9c44fe373e2b3db191a93bde8e15
Milos Stojanovic <milos.stojanovic@kelcix.com>
2017-12-26 14:47:32 +0100

    Slight changes to auth code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
-{  
+{
 
     public function token(Request $request)
-    {   
-        if($request->api_secret !== md5(env('APP_KEY') . env('APP_USER-AGENT'))) {
+    {
+        if($request->input('api_secret') == null || $request->input('api_secret') !== md5(env('APP_KEY') . env('APP_USER-AGENT'))) {
             return response()->json(['title' => 'Error', 'message' => "Unauthorized"], 401);
         }
         
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,7 +9,7 @@ class Authenticate
     public function handle($request, Closure $next)
     {
         
-        if($request->header()["authorization"][0] !== md5(env('APP_KEY') . env('APP_USER-AGENT') . env("APP_SALT"))) {
+        if($request->header("authorization") == null || $request->header()["authorization"][0] !== md5(env('APP_KEY') . env('APP_USER-AGENT') . env("APP_SALT"))) {
             return response('Unauthorized.', 401);
         }
 
